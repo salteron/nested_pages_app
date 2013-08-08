00b1d64aa964be6099a0afcd36fca304bde5035b
Michael Sogomonyan <ms.sogomonyan@gmail.com>
2013-08-08 23:55:01 +0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedPagesApp::Application.config.secret_token = 'ea2aeba86b854b417a2cf6f329073888171bd9257dfc01dc19618efabec7f236e3f481203f4a6b6e57a27d767dc764a00ede87618e5ec1a92688c5df858a2f72'
